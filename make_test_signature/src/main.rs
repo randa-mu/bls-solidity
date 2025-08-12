b9a772b5839e632b3961673f0da57a441f36400b
Julie B. <no-reply@bbjubjub.fr>
2025-08-12 18:05:34 +0200

    add bls12 381

--- make_test_signature/src/main.rs
+++ make_test_signature/src/main.rs
@@ -1,16 +1,16 @@
 use ark_ec::AffineRepr;
-use dcipher_agents::signer::BN254SignatureOnG1Signer;
+use make_test_signature::signer::BLS12_381SignatureOnG1Signer;
 
 use ark_ff::fields::Field;
-use dcipher_agents::signer::BlsSigner;
+use dcipher_signer::BlsSigner;
 use rand::RngCore;
 
-fn generate_sk() -> ark_bn254::Fr {
+fn generate_sk() -> ark_bls12_381::Fr {
     let mut rng = rand::thread_rng();
     let mut buf = [0u8; 32];
     loop {
         rng.fill_bytes(&mut buf);
-        if let Some(sk) = ark_bn254::Fr::from_random_bytes(&buf) {
+        if let Some(sk) = ark_bls12_381::Fr::from_random_bytes(&buf) {
             break sk;
         }
     }
@@ -18,15 +18,15 @@ fn generate_sk() -> ark_bn254::Fr {
 
 fn main() -> anyhow::Result<()> {
     let sk = generate_sk();
-    let pk = ark_bn254::G2Affine::generator() * sk;
+    let pk = ark_bls12_381::G2Affine::generator() * sk;
 
     let chain_id = 1;
     let dst = format!(
-        "dcipher-randomness-v01-BN254G1_XMD:KECCAK-256_SVDW_RO_0x{:064x}_",
+        "dcipher-randomness-v01-BLS12_381G1_XMD:KECCAK-256_SVDW_RO_0x{:064x}_",
         chain_id
     )
     .into_bytes();
-    let cs = BN254SignatureOnG1Signer::new(sk, dst);
+    let cs = BLS12_381SignatureOnG1Signer::new(sk, dst);
 
     let msg = "hello";
     let sig = cs.sign(msg)?;
