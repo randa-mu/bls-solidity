0c5fd76f64d5c678894676a94c3fc46a45757f05
Julie B. <no-reply@bbjubjub.fr>
2025-08-14 11:56:17 +0200

    add tests

--- test/BLS2Test.sol
+++ test/BLS2Test.sol
@@ -7,6 +7,10 @@ import {BLS} from "src/libraries/BLS.sol";
 import {BLS2} from "src/libraries/BLS2.sol";
 
 contract BLS2Test is Test {
+	function memeq(bytes memory a, bytes memory b) internal pure returns (bool) {
+		return keccak256(a) == keccak256(b);
+	}
+
     function test_sample_signature() public view {
         BLS2.PointG2 memory pk;
         BLS2.PointG1 memory sig;
@@ -19,6 +23,8 @@ pk = BLS2.PointG2(0x0736e4fdafcb1b37029b49e4fe525c6a, 0x3c982c71be446835ad74b1b8
 message = "hello";
 m_expected = BLS2.g1Unmarshal(hex"0cd00b645f411ec4d3e72182c66788c8b0933c1c020ae997cb5e9aedf08d6fc0b2968ec615c7cd05e366843a6b07b43c0eda73157f463f1a1a5af323b7489fd745b9e8d394db63dfede5af5e09dfb3754132a9977137591f57ca4b1820e5bff5");
 sig = BLS2.g1Unmarshal(hex"01c837700bce85224f0eb37aad128820b76e7156e73f3ac8aef53257e7ee0d140edb1a418d6782f19b784df994ecb6c1064f7a358c9db560b5dc6f80fcaac12f1430e74b211a61a8b4c41b0bb843b536cb774b478a296226c97c2fd3fc2390a9");
+
+assert(memeq(BLS2.g1Marshal(sig), hex"01c837700bce85224f0eb37aad128820b76e7156e73f3ac8aef53257e7ee0d140edb1a418d6782f19b784df994ecb6c1064f7a358c9db560b5dc6f80fcaac12f1430e74b211a61a8b4c41b0bb843b536cb774b478a296226c97c2fd3fc2390a9"));
 
         BLS2.PointG1 memory m = BLS2.hashToPoint(bytes(dst), bytes(message));
         console.log("m.x_hi", m.x_hi);
